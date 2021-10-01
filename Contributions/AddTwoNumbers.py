102ae3ca80d034266d6131ee677447edc437187d
Santiago Ariza <48007679+santiariza15@users.noreply.github.com>
2021-10-02 09:04:28 +1000

    Create AddTwoNumbers.py

---
+++ Contributions/AddTwoNumbers.py
@@ -0,0 +1,8 @@
+x1 = input('Enter first number: ')
+x2 = input('Enter second number: ')
+
+# Add two numbers
+y = float(x1) + float(x2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(x1, x2, y))

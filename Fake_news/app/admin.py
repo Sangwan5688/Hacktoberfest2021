d41828c33af49da81636a6847899410cc28091b8
Pratikdate <pdate73@gmail.com>
2023-10-13 16:32:01 +0530

    z

--- Fake_news/app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import FakeNews_model
-# Register your models here.
-admin.site.register(FakeNews_model)
--- Fake_news/app/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import FakeNews_model
-
-
-class FakeNews_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = FakeNews_model
-        fields =['title', 'text', 'subject','date']
-        

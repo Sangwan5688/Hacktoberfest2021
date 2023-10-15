e33e5ff49d1f448fe7f9d17290105258778e96c7
amitverma147 <vamit20000@gmail.com>
2023-10-15 14:15:32 +0530

    changed secret key

--- weather-web/weather-web-app-main/script.js
+++ weather-web/weather-web-app-main/script.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "952a732055b030068f354fac04027325"; // API key for OpenWeatherMap API
+const API_KEY = "xyz"; // API key for OpenWeatherMap API
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card

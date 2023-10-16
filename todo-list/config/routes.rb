0a26f524fc924b00d777094bf7becd9e8635c269
Ayush varshney <ayushvarshney42003@gmail.com>
2023-10-16 15:52:52 +0530

    Weather

--- todo-list/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root to: 'tasks#index'
-  resources :tasks
-end
--- todo-list/db/migrate/20170812060132_add_details_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddDetailsToTasks < ActiveRecord::Migration[5.1]
-  def change
-    add_column :tasks, :details, :text
-  end
-end
